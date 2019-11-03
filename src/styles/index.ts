3654668e39824ca7862e079e1e089ad3522c2fd8
Nathan Dimmer <nathandimmer@gmail.com>
2019-11-03 04:46:39 -0500

    QR CODES WORKING

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -45,6 +45,17 @@ export const styles = makeStyles((theme: Theme) =>
     },
     dropdownContainer: {
       padding: "20px"
+    },
+    modal: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: "2px solid #000",
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3)
     }
   })
 );
