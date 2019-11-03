bc7006e2bda4e4c3d30b9485c01533b30bc64aac
Nathan Dimmer <nathandimmer@gmail.com>
2019-11-02 22:27:07 -0400

    Finished User Input Front End

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  Grid,
+  Typography,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails
+} from "@material-ui/core";
+import LocationFrom from "./components/locationFrom";
+import { styles } from "./styles";
+import testData from "./data/TEST.json";
+import LocationTo from "./components/locationTo";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const airports: Airport[] = [
+  (testData as unknown) as Airport,
+  { name: "Detroit Metro", code: "DTW", points: {} }
+];
+
+const containerDivStyle = {
+  width: "90%",
+  margin: "5%"
+};
 
 const App: React.FC = () => {
+  const [airport, setAirport] = React.useState<Airport | null>(null);
+  const [fromID, setFromID] = React.useState<string>("");
+  const [toCategory, setToCategory] = React.useState<string>("");
+  const [toName, setToName] = React.useState<string>("");
+  const [expand, setExpand] = React.useState<boolean>(true);
+  const classes = styles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" style={containerDivStyle}>
+      <CssBaseline />
+      <ExpansionPanel expanded={expand}>
+        <ExpansionPanelSummary
+          style={{}}
+          className={classes.gradient}
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+          onClick={() => {
+            setExpand(!expand);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Typography variant="h5">
+            {!expand && fromID && toName
+              ? `From: ${fromID} To: ${toName}`
+              : "Locations"}
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            style={{ width: "100%" }}
+          >
+            <Grid item xs={12} md={6}>
+              <LocationFrom
+                airports={airports}
+                airport={airport}
+                setAirport={setAirport}
+                classes={classes}
+                fromID={fromID}
+                setFromID={setFromID}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <LocationTo
+                airports={airports}
+                airport={airport}
+                setAirport={setAirport}
+                classes={classes}
+                toCategory={toCategory}
+                setToCategory={setToCategory}
+                toName={toName}
+                setToName={setToName}
+              />
+            </Grid>
+          </Grid>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
     </div>
   );
-}
+};
 
 export default App;
